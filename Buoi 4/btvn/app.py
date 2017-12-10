fb97a45ecda2a61c064f1569244ea82179272f0b
Minh Thanh <minhthanh@Thanhs-MacBook-Pro.local>
2017-12-10 10:23:52 +0700

    HW

--- Buoi 4/btvn/app.py
+++ Buoi 4/btvn/app.py
@@ -8,13 +8,13 @@ class Item(Document):
     image = StringField()
     description = StringField()
     price = IntField()
-food = Item(
-    title = 'Món ăn Việt Nam',
-    image = 'https://media.foody.vn/res/g18/178106/prof/s640x400/foody-mobile-t1-jpg-559-635809453498630683.jpg',
-    description = 'Bánh cuốn',
-    price = 250000
-)
-food.save()
+# food = Item(
+#     title = 'Món ăn Việt Nam',
+#     image = 'https://media.foody.vn/res/g18/178106/prof/s640x400/foody-mobile-t1-jpg-559-635809453498630683.jpg',
+#     description = 'Bánh cuốn',
+#     price = 250000
+# )
+# food.save()
 @app.route('/')
 def index():
     data = Item.objects()
--- Buoi 5/app.py
+++ Buoi 5/app.py
@@ -1,46 +1,38 @@
 from flask import Flask, render_template, request
-import mlab
+import mlab;
 from mongoengine import *
-app = Flask(__name__)
 
-# 1.Connect to database
 mlab.connect()
-# 2.Design collection
 class Item(Document):
     title = StringField()
     image = StringField()
     description = StringField()
     price = IntField()
-# 3.Try insert an item
-# new_item = Item(
-#     title="Đài cũ",
-#     image="http://audioidiots.com/Sony/catalogi/1986%20hifi/middel/p037i1.jpg",
-#     description="Đài cũ giá cao",
-#     price=500000
-# )
-# new_item.save()
+
+app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    items = Item.objects()
-    return render_template('index.html', items = items)
+    data = Item.objects()
+    return render_template('index.html', items = data)
 
-@app.route('/add_item', methods=['GET','POST'])
+@app.route('/add_item', methods = ['GET', 'POST'])
 def add_item():
     if request.method == 'GET':
         return render_template('add_item.html')
     elif request.method == 'POST':
-        #1 Lấy data từ form
         form = request.form
         title = form['title']
         image = form['image']
         description = form['description']
         price = form['price']
-        #2 Thêm vào database
         new_item = Item(title = title, image = image, description = description, price = price)
         new_item.save()
+        
+    return("Đã thêm thành công")
 
-        return "Ok baby"
+
 
 if __name__ == '__main__':
   app.run(debug=True)
